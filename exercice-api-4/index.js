46df65fe7e79d79dc6c73771957a18595f8a95ec
Natalia Boudard <natalia.boudard@gmail.com>
2026-03-24 09:06:50 +0100

    fix: :zap: version v2

--- exercice-api-4/index.js
+++ exercice-api-4/index.js
@@ -3,10 +3,24 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.json({ message: "API V1 - Bonjour !", status: "success" });
+const apiRouter = express.Router();
+
+apiRouter.get('/', (req, res) => {
+    res.json({
+        message: "API V1 - Bonjour !",
+        status: "success",
+    });
 });
 
+apiRouter.get('/new-feature', (req, res) => {
+    res.json({
+        message: "Ceci est la nouvelle route de la V2 !",
+        status: "updated"
+    });
+});
+
+app.use('/api', apiRouter);
+
 app.listen(PORT, () => {
-    console.log(`Serveur lancé sur le port ${PORT}`);
+    console.log(`Serveur lancé sur http://localhost:${PORT}`);
 });
